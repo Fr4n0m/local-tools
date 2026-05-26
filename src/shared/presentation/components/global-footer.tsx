75d0e2987a7678de9e2dd61eaad5b21cddd7ae6a
DOMINATOR\Fran1 <fran11799@outlook.com>
2026-05-26 16:51:08 +0200

    🩹 fix(footer): restore missing useState import for footerTools

--- src/shared/presentation/components/global-footer.tsx
+++ src/shared/presentation/components/global-footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "@gravatar-com/hovercards/dist/style.css";
 import {
   IconAlertCircle,
